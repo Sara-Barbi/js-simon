0a35ec1782e0ba3c43119ad6b4eca8ba3b12af90
Sara-Barbi <barbisara02@gmail.com>
2021-12-29 17:41:39 +0100

    style non funzionante

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
-
 numbers = [];
 
-for (let i = 0; i < 5; i++) {                             
+for (let i = 1; i < 6; i++) {                             
 
     let randomNumb = Math.floor(Math.random()*100+1);
     numbers.push(randomNumb);
+
 }     
 
 document.getElementById("numeridaricordare").innerHTML= numbers;
@@ -15,53 +15,26 @@ function disappear(){
     
 }
 
-setTimeout(questionPrompt , 30000);
-setTimeout(disappear , 29000);
+setTimeout(disappear , 2900);
+setTimeout(questionPrompt , 3000);
 
 
 
 function questionPrompt(){
+    let i;
+    for(i=1; i<6;i++){
+        let question = prompt('scrivi il '+i+'° numero');
+        if(numbers.includes(parseInt(question))==true){
+            document.writeln('<div>'+i+'° numero corretto </div>');
+            document.querySelector("div").classList.add("green");
+            
+        }else{
+            document.writeln('<div id="red">'+i+'° numero ERRATO </div>');
+        }
+        
+        
+    }
 
-for(let i=1; i<6;i++){
-    let question = prompt('scrivi il '+i+'° numero');
-    if(numbers.includes(parseInt(question))==true){
-        document.writeln( i+'° numero corretto, ');
-    }else{
-        document.writeln( i+'° numero ERRATO, ');
-    }
-}
-/*
-    let question = prompt("scrivi il primo numero");
-    let question2 = prompt("scrivi il secondo numero");
-    let question3 = prompt("scrivi il terzo numero");
-    let question4 = prompt("scrivi il quarto numero");
-    let question5 = prompt("scrivi il quinto numero");
-    
-    if(numbers.includes(parseInt(question))==true){
-        document.writeln("primo numero corretto,");
-    }else{
-        document.writeln("primo numero NON corretto,");
-    }
-    if(numbers.includes(parseInt(question2))==true){
-        document.writeln("secondo numero corretto,");
-    }else{
-        document.writeln("secondo numero NON corretto,");
-    }
-    if(numbers.includes(parseInt(question3))==true){
-        document.writeln("terzo numero corretto,");
-    }else{
-        document.writeln("terzo numero NON corretto,");
-    }
-    if(numbers.includes(parseInt(question4))==true){
-        document.writeln("quarto numero corretto,");
-    }else{
-        document.writeln("quarto numero NON corretto,");
-    }
-    if(numbers.includes(parseInt(question5))==true){
-        document.writeln("quinto numero corretto,");
-    }else{
-        document.writeln("quinto numero NON corretto,");
-    }
-    */
+
 }
 
